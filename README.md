1b50482033ffdf7e0d51e6a6151115e8f906bcb5
mdalmamunit427 <mdalmamunit427@gmail.com>
2023-07-25 13:14:51 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# temperature-converter-Website-using-html-css-JavaScript
+![temperature-converter-Website-using-html-css-JavaScript](img/github_cover.jpeg)
---
+++ js/app.js
@@ -0,0 +1,41 @@
+// script.js
+const celsiusField = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusField.innerHTML = "";
+});
+
+if(degree.value === ""){
+  convertBtn.setAttribute("disabled","");
+  setTimeout(() => {
+    convertBtn.removeAttribute('disabled');
+  }, 4000);
+}
+
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+  convertBtn.innerHTML = "<span class='icon'><i class='fa fa-spinner fa-spin'></i> Converting...</span>";
+  setTimeout(() => {
+    convertBtn.innerHTML ="<span>Convert</span>"
+  }, 1000);
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+  
+  setTimeout( () => {
+    if (tempType.value === "fahrenheit") {
+      const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+      celsiusField.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
+    } else if (tempType.value === "kelvin") {
+      const KelvinToCelsius = inputValue - 273.15;
+      celsiusField.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;c`;
+    }
+  }, 1200)
+}
